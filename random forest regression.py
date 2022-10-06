050d44fefe7b0d9e376a56130ce2e3fb382f502b
Zarilex09 <Jiayonglin09@gmail.com>
2022-10-06 15:58:29 +0800

    ML

--- random forest regression.py
+++ random forest regression.py
@@ -1,5 +1,4 @@
-# Decision Tree Regression
-# Usually better for models with many features
+# Random Forest Regression -> ensemble of ML models and takes the average
 
 # Importing the libraries
 import numpy as np
@@ -11,24 +10,21 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
-# Training the Decision Tree Regression model on the whole dataset
-from sklearn.tree import DecisionTreeRegressor
-# Set seed as 0
-regressor = DecisionTreeRegressor(random_state = 0)
+# Training the Random Forest Regression model on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+# no of estimators =  number of trees
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X, y)
 
 # Predicting a new result
-# Double square brackets
 regressor.predict([[6.5]])
 
-# Visualising the Decision Tree Regression results (higher resolution) -> On lower resolution(plot X instead of X_grid), points are connected by curve
-# Only for 2D 
-
+# Visualising the Random Forest Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
